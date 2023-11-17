26018f17b30e8a1dc63734303eb96d118a287880
Farid Wazdi <faridwazdi02@gmail.com>
2023-11-18 00:03:22 +0700

    feat: implement api validator

--- backend/app/Http/Controllers/TransactionController.php
+++ backend/app/Http/Controllers/TransactionController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers;
 use \MoneyChanger;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class TransactionController extends Controller
 {
     public function getRate(Request $request){
         $code = $request->input('code');
-        $transaction_type = $request->input('type', 'buy');
+        $transaction_type = $request->input('type');
+        // remove default value of 'type' to make understandable
+        $validator = Validator::make($request->all(), [
+            'code' => 'required|exists:currencies,code',
+            'type' => 'required|in:buy,sell',
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'messages' => $validator->errors(),
+            ];
+            return response()->json($response, 400);
+        }
+
         $data = MoneyChanger::getExchangeRate($code, $transaction_type);
         return response()->json($data, $data['success'] === true ? 200 : 400);
     }
@@ -17,7 +33,21 @@ class TransactionController extends Controller
     public function exchange(Request $request){
         $code = $request->input('code');
         $amount = $request->input('amount');
-        $transaction_type = $request->input('type', 'buy');
+        $transaction_type = $request->input('type');
+
+        $validator = Validator::make($request->all(), [
+            'code' => 'required|exists:currencies,code',
+            'type' => 'required|in:buy,sell',
+            'amount' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'messages' => $validator->errors(),
+            ];
+            return response()->json($response, 400);
+        }
 
         $data = MoneyChanger::getExchangeRate($code, $transaction_type);
         if($data['success'] === true){
