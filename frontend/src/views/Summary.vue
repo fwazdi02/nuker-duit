c1249a36b3392399235e359ccc19a27df46e4015
Farid Wazdi <faridwazdi02@gmail.com>
2023-11-18 17:29:51 +0700

    feat: ui summary page

--- frontend/src/views/Summary.vue
+++ frontend/src/views/Summary.vue
@@ -1,7 +1,28 @@
 <script setup></script>
 
 <template>
-  <div>Summary</div>
+  <div>
+    <n-card title="Summary">
+      <n-table :bordered="false" :single-line="false">
+        <thead>
+          <tr>
+            <th>Currency</th>
+            <th>Total Buy</th>
+            <th>Total Sell</th>
+            <th>Available Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>JPY</td>
+            <td>1200</td>
+            <td>1200</td>
+            <td>7000</td>
+          </tr>
+        </tbody>
+      </n-table>
+    </n-card>
+  </div>
 </template>
 
 <style></style>
